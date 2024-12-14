e484d5a1940ec44f1c57733f2fec0833c99f96ee
Isaac0099 <isaacchamberlain01@gmail.com>
2024-12-14 14:55:49 -0700

    some small fixes and cleanup of unused code

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,11 @@
-import RefinanceCalculator from '@/components/RefinanceCalculator';
-import HomeAdder from '@/components/HomeAdder';
-import PortfolioSimulator from '@/components/PortfolioSimulator';
-
+import RefinanceCalculator from "@/components/RefinanceCalculator";
+import PortfolioSimulator from "@/components/PortfolioSimulator";
 
 export default function Home() {
   return (
     <main className="min-h-screen p-8">
       {/* <RefinanceCalculator /> */}
-      {/* <HomeAdder /> */}
       <PortfolioSimulator />
     </main>
-  )
+  );
 }
